6ef7c678da21fd07ad325f6fca33b1e3a1b79f87
hongtuanquyen <hongtuanquyen@gmail.com>
2018-11-16 15:29:45 +0700

    Add Gameplay Activity

--- app/src/main/java/com/firstapp/quyen/freakingmath_cleancode_practice/MainActivity.java
+++ app/src/main/java/com/firstapp/quyen/freakingmath_cleancode_practice/MainActivity.java
@@ -1,13 +1,37 @@
 package com.firstapp.quyen.freakingmath_cleancode_practice;
 
+
+import android.content.Intent;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button btnStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnStart = findViewById(R.id.button_Start);
+
+        btnStart.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent;
+        switch (v.getId()){
+            case R.id.button_Start:
+                intent = new Intent(MainActivity.this, GameplayActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
