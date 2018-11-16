3b8ab0f8e0348f4cb5623b5ce093a3767f248b21
hongtuanquyen <hongtuanquyen@gmail.com>
2018-11-16 17:29:09 +0700

    Fix clean code problem ver.01

--- app/src/main/java/com/firstapp/quyen/freakingmath_cleancode_practice/GameplayActivity.java
+++ app/src/main/java/com/firstapp/quyen/freakingmath_cleancode_practice/GameplayActivity.java
@@ -11,14 +11,13 @@ import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
-import java.util.ArrayList;
 import java.util.Random;
 
 public class GameplayActivity extends AppCompatActivity implements View.OnClickListener {
-    // Random random;
     TextView txtViewOperands;
     TextView txtViewResult;
     TextView txtPoint;
+    TextView txtViewTest;
     ImageView imgArgee;
     ImageView imgDisargee;
     SeekBar seekBarTimer;
@@ -31,82 +30,125 @@ public class GameplayActivity extends AppCompatActivity implements View.OnClickL
     private int operandB;
     private int point;
     private int timer_value;
-    private boolean displayRightAnswer = true;
-    private boolean displayPlusMark = true;
-    private boolean rightanswer;
-    private boolean yourchoice;
+    private boolean chooseRightAnswer;
+    private boolean choosePlusOperation;
+    private boolean rightAnswer;
+    private boolean playerChoice;
     private CountDownTimer countDownTimer;
-    private ArrayList<Integer> arrayScore;
 
-
-    TextView txtViewTest;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.screen_gameplay_layout);
+        initializeView();
+        setViewOnClickListener();
+        processValueDisplay();
+        startTimer();
+    }
+
+    public void initializeView(){
         txtViewOperands = findViewById(R.id.textView_PlusOperands);
         txtViewResult = findViewById(R.id.textView_Result);
         txtPoint = findViewById(R.id.textView_Point);
         imgArgee = findViewById(R.id.imgView_Agree);
         imgDisargee = findViewById(R.id.imgView_Disagree);
         seekBarTimer = findViewById(R.id.seekBar_Timer);
+        txtViewTest = findViewById(R.id.textView_Test);
+
         seekBarTimer.setProgress(seekBarTimer.getMax());
         timer_value = seekBarTimer.getMax();
+    }
 
-        txtViewTest = findViewById(R.id.textView_Test);
-        arrayScore = new ArrayList<>();
+    public void setViewOnClickListener(){
         imgArgee.setOnClickListener(this);
         imgDisargee.setOnClickListener(this);
-        startTimer();
-        displayNumbers();
+    }
 
+    public void processValueDisplay(){
+        prepareNumbers();
+
+        //Xử lí quyết định hiển thị đúng hay sai kết quả
+        Random random = new Random();
+        chooseRightAnswer = random.nextBoolean();
+        if(chooseRightAnswer){
+            rightAnswer = true;
+        }
+        else{
+            rightAnswer = false;
+            // Xử lí kết quả là tổng hay hiệu
+            choosePlusOperation = random.nextBoolean();
+            if(choosePlusOperation){
+                result += random.nextInt(2) + 1;
+            }
+            else{
+                result -= random.nextInt(2) + 1;
+                if(result == 1 || result == 0)
+                    result += 1;
+            }
+        }
+        displayNumbers();
+    }
+
+    public void prepareNumbers(){
+        Random random = new Random();
+        result = random.nextInt(40) + 2;
+        operandA = result - (random.nextInt(result -1 ) +1);
+        operandB = result - operandA;
+    }
+
+    public  void displayNumbers(){
+        txtViewOperands.setText(operandA + " + " + operandB);
+        txtViewResult.setText("= " + result);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.imgView_Agree:
-                yourchoice = true;
-                if(yourchoice == rightanswer){
-                    setAnotherRound();
-                }
-                else{
-                    txtViewTest.setText(arrayScore.get(0).toString() + " " + arrayScore.get(1).toString() + " " + arrayScore.get(2).toString() + " " + arrayScore.get(3).toString() + " " + arrayScore.get(4).toString() + " ");
-                    displayGameOverDialog();
-                }
+                playerChoice = true;
+                processPlayerChoice(playerChoice);
                 break;
             case R.id.imgView_Disagree:
-                yourchoice = false;
-                if(yourchoice == rightanswer){
-                    setAnotherRound();
-                }
-                else{
-                    txtViewTest.setText(arrayScore.get(0).toString() + " " + arrayScore.get(1).toString() + " " + arrayScore.get(2).toString() + " " + arrayScore.get(3).toString() + " " + arrayScore.get(4).toString() + " ");
-                    displayGameOverDialog();
-                }
+                playerChoice = false;
+                processPlayerChoice(playerChoice);
                 break;
         }
     }
 
-    public void setAnotherRound(){
+    public void processPlayerChoice(boolean playerChoice){
+        if(playerChoice == rightAnswer){
+            moveToAnotherRound();
+        }
+        else{
+            countDownTimer.cancel();
+            displayGameOverDialog();
+        }
+    }
+
+    public void moveToAnotherRound(){
+        //Update player's point
         point++;
         txtPoint.setText("Point: " + point);
-        displayNumbers();
+
+        processValueDisplay();
+
+        // Reset timer
         countDownTimer.cancel();
         timer_value = seekBarTimer.getMax();
         startTimer();
     }
+
     public void startTimer(){
         countDownTimer = new CountDownTimer(2000, 100) {
             @Override
             public void onTick(long millisUntilFinished) {
+                // Update timer bar after 100ms
                 timer_value = timer_value - (seekBarTimer.getMax()*100/2000);
                 seekBarTimer.setProgress(timer_value);
             }
 
             @Override
             public void onFinish() {
-                txtViewTest.setText(arrayScore.get(0).toString() + " " + arrayScore.get(1).toString() + " " + arrayScore.get(2).toString() + " " + arrayScore.get(3).toString() + " " + arrayScore.get(4).toString() + " ");
                 displayGameOverDialog();
             }
         }.start();
@@ -118,17 +160,15 @@ public class GameplayActivity extends AppCompatActivity implements View.OnClickL
         dialog.setCanceledOnTouchOutside(false);
         dialog.setContentView(R.layout.dialog_gameover_layout);
         dialog.show();
-        countDownTimer.cancel();
         btnMainMenu = dialog.findViewById(R.id.button_MainMenu);
         btnRestart = dialog.findViewById(R.id.button_Restart);
-
         btnRestart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 dialog.dismiss();
                 point = 0;
                 txtPoint.setText("Point: " + point);
-                displayNumbers();
+                processValueDisplay();
                 countDownTimer.cancel();
                 timer_value = seekBarTimer.getMax();
                 startTimer();
@@ -144,48 +184,10 @@ public class GameplayActivity extends AppCompatActivity implements View.OnClickL
         });
     }
 
-    public void displayNumbers(){
-        setValue();
-        Random random = new Random();
-        displayRightAnswer = random.nextBoolean();
-        if(displayRightAnswer){
-            setTextView();
-            rightanswer = true;
-        }
-        else{
-            rightanswer = false;
-            displayPlusMark = random.nextBoolean();
-            if(displayPlusMark){
-                result += random.nextInt(2) + 1;
-                setTextView();
-            }
-            else{
-                result -= random.nextInt(2) + 1;
-                if(result == 1 || result == 0)
-                    result += 1;
-                setTextView();
-            }
-        }
-    }
-
-    public  void setTextView(){
-        txtViewOperands.setText(operandA + " + " + operandB);
-        txtViewResult.setText("= " + result);
-    }
-    public void setValue(){
-        Random random = new Random();
-        result = random.nextInt(40) + 2;
-        operandA = result - (random.nextInt(result -1 ) +1);
-        operandB = result - operandA;
-    }
-
-
     @Override
     public void onBackPressed() {
         countDownTimer.cancel();
         finish();
         super.onBackPressed();
     }
-
-
 }
--- app/src/main/java/com/firstapp/quyen/freakingmath_cleancode_practice/MainActivity.java
+++ app/src/main/java/com/firstapp/quyen/freakingmath_cleancode_practice/MainActivity.java
@@ -1,12 +1,9 @@
 package com.firstapp.quyen.freakingmath_cleancode_practice;
 
-
 import android.content.Intent;
-
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
@@ -16,20 +13,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initializeView();
+        setViewOnClickListerner();
+    }
 
-        btnStart = findViewById(R.id.button_Start);
-
+    public void setViewOnClickListerner(){
         btnStart.setOnClickListener(this);
+    }
 
-
+    public void initializeView(){
+        btnStart = findViewById(R.id.button_Start);
     }
 
     @Override
     public void onClick(View v) {
-        Intent intent;
         switch (v.getId()){
             case R.id.button_Start:
-                intent = new Intent(MainActivity.this, GameplayActivity.class);
+                Intent intent = new Intent(MainActivity.this, GameplayActivity.class);
                 startActivity(intent);
                 break;
         }
